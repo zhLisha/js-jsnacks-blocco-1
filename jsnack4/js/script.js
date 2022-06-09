04932f922cbc37881d78adf501de1c37a5614103
zhLisha <lisazhou1709@gmail.com>
2022-06-09 14:39:12 +0200

    snack4 done

---
+++ jsnack4/js/script.js
@@ -0,0 +1,21 @@
+/* 
+- Chiedere all'utente 6 volte di inserire un numero 
+- Se e' dispari inserire nell'array
+*/ 
+
+// Array vuoto  da riempire
+const userNumberOdd = [];
+
+
+// Richiesta numeri all'utente
+for( let i = 0; i < 6; i++) {
+    const userNumber = prompt( 'Scrivi un numero per 6 volte' );
+
+    // Se dispari popolare nell'ARRAY userNumberOdd
+    if(userNumber % 2 !== 0) {
+        userNumberOdd.push(userNumber);
+    } 
+}
+
+
+
