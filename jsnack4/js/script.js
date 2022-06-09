95080afbe70fdc7a7ff4845cb998a4e6cd97b4d9
zhLisha <lisazhou1709@gmail.com>
2022-06-09 14:45:58 +0200

    edidt snack4 to parseInt

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -9,13 +9,14 @@ const userNumberOdd = [];
 
 // Richiesta numeri all'utente
 for( let i = 0; i < 6; i++) {
-    const userNumber = prompt( 'Scrivi un numero per 6 volte' );
+    const userNumber = parseInt( prompt( 'Scrivi un numero per 6 volte' ) );
 
     // Se dispari popolare nell'ARRAY userNumberOdd
     if(userNumber % 2 !== 0) {
         userNumberOdd.push(userNumber);
     } 
 }
+
 
 
 
